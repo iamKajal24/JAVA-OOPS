2bf0ec559ecaad719bdcc1762be0721a9a7d4e34
Kajal <113601134+iamKajal24@users.noreply.github.com>
2024-01-15 21:50:51 +0530

    Add files via upload

---
+++ MethodOverLoadingChangingNoArgument.java
@@ -0,0 +1,20 @@
+package OOPS;
+
+class Adder {
+	static int add(int a, int b) {
+		return a + b;
+	}
+
+	static int add(int a, int b, int c) {
+		return a + b + c;
+	}
+}
+
+public class MethodOverLoadingChangingNoArgument {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Adder.add(11, 22));
+		System.out.println(Adder.add(11, 22, 33));
+	}
+}
