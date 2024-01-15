047effb0a6a53c420420c6c008270bef7fcd1417
Kajal <113601134+iamKajal24@users.noreply.github.com>
2024-01-15 22:00:27 +0530

    Add files via upload

---
+++ MethodOverloading2.java
@@ -0,0 +1,18 @@
+package OOPS;
+class Adder1{
+	static int add(int a,int b){
+		return a+b;
+	}
+	static double add(double a ,double b) {
+		return a+b;
+	}
+}
+public class MethodOverloading2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println(Adder.add(11,22));
+       System.out.println(Adder.add(12.3,34.5));
+	}
+
+}
