3312e6b6c7e2a6f331a2b17d446921e56271e163
KAJAL <113601134+iamKajal24@users.noreply.github.com>
2024-01-24 18:07:14 +0530

    Add files via upload

---
+++ CanWeOverloadJavaMain.java
@@ -0,0 +1,17 @@
+package OOPS;
+
+public class CanWeOverloadJavaMain {
+
+	public static void main(String[] args) {
+		System.out.println("main the string[]");
+
+	}
+
+	public static void main(String args) {
+		System.out.println("main the String");
+	}
+
+	public static void main() {
+		System.out.println("main without agrs");
+	}
+}
