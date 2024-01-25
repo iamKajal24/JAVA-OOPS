e2b1ba2fbf119b69a915b94a99120e2f64da279f
KAJAL <113601134+iamKajal24@users.noreply.github.com>
2024-01-25 20:56:55 +0530

    Add files via upload

---
+++ StaticBinding.java
@@ -0,0 +1,16 @@
+package javaOOPS;
+
+class Dog58 {
+	final void eat65() {
+		System.out.println("Dog is Eating....");
+	}
+}
+
+public class StaticBinding {
+
+	public static void main(String[] args) {
+		Dog58 obj = new Dog58();
+		obj.eat65();
+
+	}
+}
