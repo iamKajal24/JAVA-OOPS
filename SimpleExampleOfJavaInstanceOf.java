b07ed1f8d3fe51a8da2bc87e94b5d7589c9333f8
KAJAL <113601134+iamKajal24@users.noreply.github.com>
2024-01-25 21:05:47 +0530

    Add files via upload

---
+++ SimpleExampleOfJavaInstanceOf.java
@@ -0,0 +1,15 @@
+package javaOOPS;
+
+class simple1 {
+	
+}
+
+	public class SimpleExampleOfJavaInstanceOf {
+
+		public static void main(String[] args) {
+			simple1 s = new simple1();
+			System.out.println(s instanceof simple1);
+		}
+
+	}
+
