defef3837f44442beafa5a84a13079d17509c0b3
KAJAL <113601134+iamKajal24@users.noreply.github.com>
2024-01-25 21:11:33 +0530

    Add files via upload

---
+++ InstanceInJavaWithAVariableThatHaveNullHave.java
@@ -0,0 +1,13 @@
+package javaOOPS;
+
+class Dog2 {
+
+}
+
+public class InstanceInJavaWithAVariableThatHaveNullHave {
+
+	public static void main(String[] args) {
+		Dog2 d = null;
+		System.out.println(d instanceof Dog2);
+	}
+}
